d1c5f9e8613d85696153bab7ad26b2e4fb6b899c
Duncan Rose <mail@duncanrose.com>
2015-03-30 13:50:13 -0400

    Validations

--- main/app/models/movie.rb
+++ main/app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :user_id,
+    presence: true,
+    numericality: true
 end
--- main/app/models/user.rb
+++ main/app/models/user.rb
@@ -1,6 +1,29 @@
 class User < ActiveRecord::Base
+  has_many :movies
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-end
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :sign_in_count, presence: true
+  validates :user_name, presence: true
+  validates :admin, presence: true
+  end
+
+
+t.string   "email",                  default: "", null: false
+t.string   "encrypted_password",     default: "", null: false
+t.string   "reset_password_token"
+t.datetime "reset_password_sent_at"
+t.datetime "remember_created_at"
+t.integer  "sign_in_count",          default: 0,  null: false
+t.datetime "current_sign_in_at"
+t.datetime "last_sign_in_at"
+t.inet     "current_sign_in_ip"
+t.inet     "last_sign_in_ip"
+t.datetime "created_at"
+t.datetime "updated_at"
+t.string   "user_name",                           null: false
+t.boolean  "admin",                               null: false

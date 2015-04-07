de1ce75cd04b12cea60b02f7780d8ca0b71b1b3a
Jack <jack.sy@live.com>
2015-04-07 10:39:10 -0400

    setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,7 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require foundation
 //= require_tree .
-
-
 
 $(function() {
   $('.flash').delay(1500).fadeOut(600);
@@ -89,4 +75,3 @@ $('.vote-down a').on('click', function(){
     alert('You\'ve already voted');
   });
 });
-

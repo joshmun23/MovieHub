c004dc19bb149b2d6ac251da850eb23cd8756093
Duncan Rose <DuncanRose@users.noreply.github.com>
2015-04-09 10:13:37 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-50.times do |i|
-  Movie.create!(title: "Movie #{i}", year: (1960+i).to_s, user_id: 1)
-end

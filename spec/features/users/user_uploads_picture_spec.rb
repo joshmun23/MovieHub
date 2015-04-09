9575d854fd8adc03e2843fd56714b493d29fbbb8
Ezequiel Jose Lopez <skiel.j.lopez@gmail.com>
2015-04-09 15:18:07 -0400

    testing for profile image in page

--- spec/features/users/user_uploads_picture_spec.rb
+++ spec/features/users/user_uploads_picture_spec.rb
@@ -11,21 +11,21 @@ feature 'user uploads picture', %{
   # * If I don't upload a photo I can still create an account.
   # * I can edit my profile picture.
 
-  scenario 'adds a profile picture', focus: true do
+  scenario 'adds a profile picture' do
+    user = FactoryGirl.build(:user)
+
     visit new_user_registration_path
 
-    fill_in 'User name', with: 'john'
-    fill_in 'Email', with: 'john@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in 'User name', with: user.user_name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
 
-    attach_file("#{Rails.root}/app/assets/images/reel.jpeg")
-
+    attach_file("user_profile_photo", "#{Rails.root}/app/assets/images/reel.jpeg")
     click_button 'Sign up'
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(page).to have_content('Sign Out')
-    save_and_open_page
-    expect(page).to have_css("img", src: "/public/images/foo.png")
+    expect(find('.profile-image')['src']).to have_content "reel.jpeg"
   end
 end
